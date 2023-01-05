659ccaadcd944a2a98e35a905b0c2ece1cea01f8
owenayora <ayoraowen@gmail.com>
2023-01-05 22:30:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -20,10 +20,20 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+  Declare a variable to store the reversed word
+  Iterate through the passed in word string storing each character in a variable that will store the reversed word
+  Compare the reversed word with the passed in word to check if their values are equivalent
+  Return a boolean based on evaluation of the comparison
+
 */
 
 /*
   Add written explanation of your solution here
+  The function isPalindrome() takes in a string argument
+  A for loop iterates through the passed in string which is an array starting from the last index to the first in order to reverse the string
+  The loop stores each character in a varible declared to hold the reversed word
+  The function then compares the passed in word string with the reversed string 
+
 */
 
 // You can run `node index.js` to view these console logs
